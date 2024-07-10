5068f50f63676439e98938bc3d6b2ad4df7b359d
rajanbajaj <rajanbajajkota@gmail.com>
2024-07-10 12:27:56 +0530

    Add Chaikin Money Flow (CMF) indicator method to TechnicalIndicators module

--- src/rajan_nse/TechnicalIndicators.py
+++ src/rajan_nse/TechnicalIndicators.py
@@ -43,3 +43,32 @@ class TechnicalIndicators:
         # rsi = np.concatenate((np.full(period-1, np.nan), rsi))
         
         return rsi
+    
+    """
+    Chaikin Money Flow (CMF) developed by Marc Chaikin is a volume-weighted
+    average of accumulation and distribution over a specified period. The standard
+    CMF period is 21 days. The principle behind the Chaikin Money Flow is the nearer
+    the closing price is to the high, the more accumulation has taken place.
+    Conversely, the nearer the closing price is to the low, the more distribution
+    has taken place. If the price action consistently closes above the bar's midpoint
+    on increasing volume, the Chaikin Money Flow will be positive. Conversely, if the
+    price action consistently closes below the bar's midpoint on increasing volume, the
+    Chaikin Money Flow will be a negative value.
+    Calculations:
+    CMF = n-day Sum of [(((C - L) - (H - C)) / (H - L)) x Vol] / n-day Sum of Vol
+    Where: n = number of periods, typically 21 H = high L = low C = close Vol = volume
+    """
+    def cmf(self, data, period = 21):
+        sum_a = 0
+        sum_b = 0
+        data_length = len(data)
+        print(data_length, period)
+        if (period < data_length):
+            for i in range(0, period):
+                sum_a += (((data[i]['CH_CLOSING_PRICE'] - data[i]['CH_TRADE_LOW_PRICE'] ) - (data[i]['CH_TRADE_HIGH_PRICE']  - data[i]['CH_CLOSING_PRICE'] )) / (data[i]['CH_TRADE_HIGH_PRICE']  - data[i]['CH_TRADE_LOW_PRICE'] )) * data[i]['CH_TOT_TRADED_QTY']
+                sum_b += data[i]['CH_TOT_TRADED_QTY']
+        else:
+            for tmp in data:
+                sum_a += (((tmp['CH_CLOSING_PRICE'] - tmp['CH_TRADE_LOW_PRICE'] ) - (tmp['CH_TRADE_HIGH_PRICE']  - tmp['CH_CLOSING_PRICE'] )) / (tmp['CH_TRADE_HIGH_PRICE']  - tmp['CH_TRADE_LOW_PRICE'] )) * tmp['CH_TOT_TRADED_QTY'] 
+                sum_b += tmp['CH_TOT_TRADED_QTY']
+        return sum_a / sum_b
