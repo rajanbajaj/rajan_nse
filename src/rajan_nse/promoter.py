e3807e24b77ae97aeb35753d4ef2bc8fa48c0740
rajanbajaj <rajanbajajkota@gmail.com>
2024-07-10 12:06:45 +0530

    Update example file with latest methods

--- src/rajan_nse/promoter.py
+++ src/rajan_nse/promoter.py
@@ -1,15 +1,18 @@
-from Strategies import Strategies
+from rajan_nse.Strategies import Strategies
+from rajan_nse.TechnicalIndicators import TechnicalIndicators
+from rajan_nse.NseData import NseData
 from pandas import DataFrame
-from rajan_nse.CandleStickPatterns import CandleStickPatterns
+
+# TODO add rajan.nse
+from CandleStickPatterns import CandleStickPatterns
 
 if __name__ == "__main__":
     # strategies = Strategies()
-
     # data = strategies.promoterBuyBackStocks(300)
     # print(data)
     # DataFrame.to_csv(DataFrame(data), 'final.csv')
     
-    
+    # strategies = Strategies()
     # data = strategies.insderTradingData("UDS", 200)
     # if not data.empty:
     #     print(data[["date", "symbol", "secVal", "secAcq", "tdpTransactionType", "secType", "personCategory"]])
@@ -22,6 +25,16 @@ if __name__ == "__main__":
     # result = candleStickPatterns.bullishEngullfingPattern('BIGBLOC')
     # print(result)
 
-    candleStickPatterns = CandleStickPatterns()
-    result = candleStickPatterns.dojiPattern('ASIANPAINT', False)
-    print(result)
+    # candleStickPatterns = CandleStickPatterns()
+    # result = candleStickPatterns.dojiPattern('ASIANPAINT', False)
+    # print(result)
+
+    technicalIndicators = TechnicalIndicators()
+    nseData = NseData()
+
+    data = nseData.getHistoricalData('RELIANCE', delta=14)
+    print(technicalIndicators.sma(data['data']))
+    data = nseData.getHistoricalData('RELIANCE')
+    print(technicalIndicators.sma(data['data'], 14))
+    print(technicalIndicators.rsi(data['data']))
+
