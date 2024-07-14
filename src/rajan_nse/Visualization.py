e75b1b144faed92d02debf892db8f9cb832130c5
rajanbajaj <rajanbajajkota@gmail.com>
2024-07-14 16:19:11 +0530

    Add Visualisation class with distribution plot method

---
+++ src/rajan_nse/Visualization.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Visualization:
+    def plotDistribution(self, data):
+        # Plotting using seaborn
+        plt.figure(figsize=(10, 6))
+        sns.histplot(data, kde=True, bins=30, color='blue', edgecolor='black')
+        plt.title('Distribution Plot of CH_TOT_TRADED_QTY')
+        plt.xlabel('CH_TOT_TRADED_QTY')
+        plt.ylabel('Frequency')
+        plt.grid(True)
+        plt.show()
