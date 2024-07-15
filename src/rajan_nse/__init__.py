e4974911fb27df49e8b7edcc846498c117ef3a5c
rajanbajaj <rajanbajajkota@gmail.com>
2024-07-15 22:12:28 +0530

    Easy import classes by adding it to init file

---
+++ src/rajan_nse/__init__.py
@@ -0,0 +1,23 @@
+"""
+rajan_nse package
+
+The rajan_nse package is a comprehensive toolkit designed for analyzing and visualizing stock market data from the National Stock Exchange (NSE) of India. This package provides various modules to facilitate technical analysis, identify candlestick patterns, create visual representations, and develop trading strategies.
+"""
+
+# Import specific functions or classes for easy access
+from .NseData import NseData
+from .Session import Session
+from .Strategies import Strategies
+from .TechnicalIndicators import TechnicalIndicators
+from .Visualization import Visualization
+
+
+# Define what should be accessible when 'from rajan_nse import *' is used
+__all__ = [
+    'NseData', 
+    'Session', 
+    'Strategies', 
+    'TechnicalIndicators', 
+    'Visualization'
+]
+
