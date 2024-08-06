7547d5bf4025e73172c7f03ad51346a7f715dc65
rajanbajaj <rajanbajajkota@gmail.com>
2024-08-06 11:28:55 +0530

    Add docstrings for NseData module

--- src/rajan_nse/NseData.py
+++ src/rajan_nse/NseData.py
@@ -7,6 +7,51 @@ class NseData:
         pass
 
     def getInsiderTradingDataWithSymbol(self, symbol, delta = 90):
+        """
+        Get insider trainind data for a given symbol.
+
+        Keywork arguments:
+        symbol -- The symbol of the stock.
+        delta -- The number of days to fetch data for. Default is 90 days.
+
+        Response structure:
+        [
+            {
+                "symbol": "COMPANY",
+                "company": "COMPANY",
+                "anex": "7(2)",
+                "acqName": "SHOBHA AGARWAL",
+                "date": "14-Dec-2023 18:02",
+                "pid": "1157172",
+                "tkdAcqm": null,
+                "buyValue": "0",
+                "sellValue": "0",
+                "buyQuantity": "0",
+                "sellquantity": "0",
+                "secType": "Equity Shares",
+                "secAcq": "150000",
+                "did": "489950",
+                "tdpTransactionType": "Sell",
+                "tdpDerivativeContractType": "-",
+                "xbrl": "https://nsearchives.nseindia.com/corporate/xbrl/IT_1157172_997084_14122023060254_WEB.xml",
+                "personCategory": "Immediate relative",
+                "befAcqSharesNo": "731905",
+                "befAcqSharesPer": "0.01",
+                "secVal": "0",
+                "securitiesTypePost": "Equity Shares",
+                "afterAcqSharesNo": "581905",
+                "afterAcqSharesPer": "0.01",
+                "acqfromDt": "11-Dec-2023",
+                "acqtoDt": "11-Dec-2023",
+                "intimDt": "12-Dec-2023",
+                "acqMode": "Gift",
+                "derivativeType": "-",
+                "exchange": "NA",
+                "remarks": "-"
+            },
+            { ... }
+        ]
+        """
         to_date = date.today()
         from_date = to_date - timedelta(days=delta)
         to_date_formated = to_date.strftime("%d-%m-%Y")
@@ -25,7 +70,167 @@ class NseData:
         return data['data']
 
     def getCurrentData(self, symbol):
-        # https://www.nseindia.com/api/quote-equity?symbol=X
+        """Get current market data for the symbol and return the json response
+        
+        Keyword arguments:
+        symbol -- the symbol of the stock
+
+        Request url: https://www.nseindia.com/api/quote-equity?symbol=X
+        Response structure:
+        {
+            "info": {
+                "symbol": "X",
+                "companyName": "X Limited",
+                "industry": "COMPUTERS - SOFTWARE",
+                "activeSeries": [
+                "EQ"
+                ],
+                "debtSeries": [],
+                "isFNOSec": true,
+                "isCASec": false,
+                "isSLBSec": true,
+                "isDebtSec": false,
+                "isSuspended": false,
+                "tempSuspendedSeries": [],
+                "isETFSec": false,
+                "isDelisted": false,
+                "isin": "INE075A01022",
+                "isMunicipalBond": false,
+                "isTop10": false,
+                "identifier": "XEQN"
+            },
+            "metadata": {
+                "series": "EQ",
+                "symbol": "X",
+                "isin": "INE075A01022",
+                "status": "Listed",
+                "listingDate": "08-Nov-1995",
+                "industry": "Computers - Software & Consulting",
+                "lastUpdateTime": "02-Jul-2024 15:24:00",
+                "pdSectorPe": 24.77,
+                "pdSymbolPe": 24.77,
+                "pdSectorInd": "NIFTY IT                                          "
+            },
+            "securityInfo": {
+                "boardStatus": "Main",
+                "tradingStatus": "Active",
+                "tradingSegment": "Normal Market",
+                "sessionNo": "-",
+                "slb": "Yes",
+                "classOfShare": "Equity",
+                "derivatives": "Yes",
+                "surveillance": {
+                "surv": null,
+                "desc": null
+                },
+                "faceValue": 2,
+                "issuedSize": 5230164205
+            },
+            "sddDetails": {
+                "SDDAuditor": "-",
+                "SDDStatus": "-"
+            },
+            "priceInfo": {
+                "lastPrice": 537.55,
+                "change": 10.199999999999932,
+                "pChange": 1.9341992983786729,
+                "previousClose": 527.35,
+                "open": 529.3,
+                "close": 0,
+                "vwap": 538.33,
+                "lowerCP": "474.65",
+                "upperCP": "580.05",
+                "pPriceBand": "No Band",
+                "basePrice": 527.35,
+                "intraDayHighLow": {
+                "min": 528.3,
+                "max": 545,
+                "value": 537.55
+                },
+                "weekHighLow": {
+                "min": 375.05,
+                "minDate": "26-Oct-2023",
+                "max": 545.9,
+                "maxDate": "19-Feb-2024",
+                "value": 537.55
+                },
+                "iNavValue": null,
+                "checkINAV": false
+            },
+            "industryInfo": {
+                "macro": "Information Technology",
+                "sector": "Information Technology",
+                "industry": "IT - Software",
+                "basicIndustry": "Computers - Software & Consulting"
+            },
+            "preOpenMarket": {
+                "preopen": [
+                {
+                    "price": 474.65,
+                    "buyQty": 0,
+                    "sellQty": 604
+                },
+                {
+                    "price": 480,
+                    "buyQty": 0,
+                    "sellQty": 500
+                },
+                {
+                    "price": 501,
+                    "buyQty": 0,
+                    "sellQty": 7161
+                },
+                {
+                    "price": 503.6,
+                    "buyQty": 0,
+                    "sellQty": 10
+                },
+                {
+                    "price": 529.3,
+                    "buyQty": 0,
+                    "sellQty": 0,
+                    "iep": true
+                },
+                {
+                    "price": 551.05,
+                    "buyQty": 306,
+                    "sellQty": 0
+                },
+                {
+                    "price": 551.1,
+                    "buyQty": 8,
+                    "sellQty": 0
+                },
+                {
+                    "price": 580,
+                    "buyQty": 20,
+                    "sellQty": 0
+                },
+                {
+                    "price": 580.05,
+                    "buyQty": 205,
+                    "sellQty": 0
+                }
+                ],
+                "ato": {
+                "buy": 24235,
+                "sell": 2858
+                },
+                "IEP": 529.3,
+                "totalTradedVolume": 98656,
+                "finalPrice": 529.3,
+                "finalQuantity": 98656,
+                "lastUpdateTime": "02-Jul-2024 09:08:03",
+                "totalBuyQuantity": 79562,
+                "totalSellQuantity": 304045,
+                "atoBuyQty": 24235,
+                "atoSellQty": 2858,
+                "Change": 1.9499999999999318,
+                "perChange": 0.3697733952782652,
+                "prevClose": 527.35
+            }
+        }
+        """
         data = self.session.makeRequest(
             url = "https://www.nseindia.com/api/quote-equity",
             params = {
@@ -37,9 +242,16 @@ class NseData:
         return data
 
     def getHistoricalData(self, symbol, delta = 200, to_date = date.today(), depth = 3):
-        # https://www.nseindia.com/api/historical/cm/equity?symbol=BAJFINANCE&series=["EQ"]&from=30-06-2024&to=01-07-2024
-        # get quote historical details
+        """Get historical data for a given symbol.
 
+        Keyword arguments:
+        symbol -- the symbol of the stock
+        delta -- the number of days to get data for (default 200)
+        to_date -- the date to get data up to (default today)
+        depth -- the number of loop count to get data for (default 3, i.e 3*200 count)
+        
+        Request url: https://www.nseindia.com/api/historical/cm/equity?symbol=SYMBOL&series=["EQ"]&from=FROM&to=TO
+        """
         from_date = to_date - timedelta(days=delta)
         to_date_formated = to_date.strftime("%d-%m-%Y")
         from_date_formated = from_date.strftime("%d-%m-%Y")
@@ -87,6 +299,20 @@ class NseData:
         return data
     
     def fiftyTwoWeekHighLow(self, symbol, live=True):
+        """
+        Get 52 Weeks High and Low values for given symbol
+
+        Keyword arguments:
+        symbol -- symbol of the stock
+        live -- boolean value to get data from live or historical data
+
+        Return structure:
+        {
+            'high': 100,
+            'low': 15,
+            'price': 55
+        }
+        """
         current_data = self.getCurrentData(symbol)
         if live:
             return {
